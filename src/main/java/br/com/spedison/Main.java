b0bb4a6a0c80230c12f41fbc5428a70b6962cd32
spedison <spedison@gmail.com>
2024-10-09 10:16:13 -0300

    Parse Json enquanto faz o download.

--- src/main/java/br/com/spedison/Main.java
+++ src/main/java/br/com/spedison/Main.java
@@ -1,27 +1,50 @@
 package br.com.spedison;
 
+import br.com.spedison.vo.Candidato;
+import br.com.spedison.vo.DadosJson;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    static final List<Candidato> allCandids = new ArrayList<>();
+
+    static class RodaProcessa implements Consumer<DadosJson> {
+        @Override
+        public void accept(DadosJson s) {
+            ParserJson p = new ParserJson(s);
+            List<Candidato> k = p.fromJsonString(s.getDadosArquivo());
+            allCandids.addAll(k);
+        }
+
+    }
+
     public static void main(String[] args) {
 
         try {
-            DownloadArquivo.downloadAll();
+            DownloadArquivo dw = new DownloadArquivo();
+            dw.setProcessadorJson(new RodaProcessa());
+            dw.downloadAll();
+
+            allCandids.forEach(System.out::println);
+            long quantidadeDeNulos =
+            allCandids.
+                    stream().
+                    filter(p-> Objects.isNull(p.getQuantidadeVotos())).
+                            count();
+            long total = allCandids.size();
+
+            System.out.println("Temos %d com %d nulos".formatted(total, quantidadeDeNulos));
+
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-
-
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
     }
 }
